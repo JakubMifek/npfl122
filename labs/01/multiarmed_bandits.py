cf35cac66e106d468d52f57694fd2a8d32b8483f
Jakub Mifek <jakubmifek@gmail.com>
2019-10-15 14:42:43 +0200

    bandits

--- labs/01/multiarmed_bandits.py
+++ labs/01/multiarmed_bandits.py
@@ -21,32 +21,37 @@ class MultiArmedBandits():
 
     def step(self, action):
         if self._done:
-            raise ValueError("Cannot step in MultiArmedBandits when there is no running episode")
+            raise ValueError('Cannot step in MultiArmedBandits when there is no running episode')
         self._trials += 1
         self._done = self._trials == self._episode_length
         reward = self._generator.normal(self._bandits[action], 1.)
         return None, reward, self._done, {}
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--bandits", default=10, type=int, help="Number of bandits.")
-parser.add_argument("--episodes", default=100, type=int, help="Training episodes.")
-parser.add_argument("--episode_length", default=1000, type=int, help="Number of trials per episode.")
-parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument('--bandits', default=10, type=int, help='Number of bandits.')
+parser.add_argument('--episodes', default=100, type=int, help='Training episodes.')
+parser.add_argument('--episode_length', default=1000, type=int, help='Number of trials per episode.')
+parser.add_argument('--seed', default=42, type=int, help='Random seed.')
 
-parser.add_argument("--mode", default="gradient", type=str, help="Mode to use -- greedy, ucb and gradient.")
-parser.add_argument("--alpha", default=0, type=float, help="Learning rate to use (if applicable).")
-parser.add_argument("--c", default=1, type=float, help="Confidence level in ucb (if applicable).")
-parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor (if applicable).")
-parser.add_argument("--initial", default=0, type=float, help="Initial value function levels (if applicable).")
+parser.add_argument('--mode', default='gradient', type=str, help='Mode to use -- greedy, ucb and gradient.')
+parser.add_argument('--alpha', default=0, type=float, help='Learning rate to use (if applicable).')
+parser.add_argument('--c', default=1, type=float, help='Confidence level in ucb (if applicable).')
+parser.add_argument('--epsilon', default=0.1, type=float, help='Exploration factor (if applicable).')
+parser.add_argument('--initial', default=0, type=float, help='Initial value function levels (if applicable).')
 
 def greedyParams(args):
-    return np.ones((2, args.bandits)) * args.initial
+    return np.ones((args.bandits, 2)) * args.initial
 
 def ucbParams(args):
     return np.ones((args.bandits, 2)) * args.initial
 
 def gradientParams(args):
-    pass
+    return np.zeros((args.bandits,))
+
+def distribution(params):
+    E = np.exp(params)
+    D = sum(E)
+    return E / D
 
 mode = {
     'greedy': greedyParams,
@@ -70,42 +75,40 @@ def main(args):
         steps = 0
         done = False
         while not done:
-            # TODO: Action selection according to mode
-            if args.mode == "greedy":
+            if args.mode == 'greedy':
                 e = np.random.rand()
                 if e < args.epsilon:
                     action = np.random.randint(0, args.bandits)
                 else:
-                    action = np.where(params[0,] == max(params[0,]))[0][0]
-            elif args.mode == "ucb":
+                    actions = np.fromiter(map(lambda val: val[0], params), dtype='float')
+                    action = np.where(actions == max(actions))[0][0]
+            elif args.mode == 'ucb':
                 actions = np.fromiter(map(lambda val: np.inf if val[1] == 0 else val[0] + args.c * np.sqrt(np.log(steps) / val[1]), params), dtype='float')
                 action = np.where(actions == max(actions))[0][0]
-            elif args.mode == "gradient":
-                action = 0
+            elif args.mode == 'gradient':
+                d = distribution(params)
+                p = np.random.multinomial(1, d)
+                action = np.where(p == 1)[0][0]
 
             _, reward, done, _ = env.step(action)
 
             rewards[episode] += reward
             steps += 1
 
-            # TODO: Update parameters
-            if args.mode == 'greedy':
-                params[1][action] += 1
+            if args.mode == 'greedy' or args.mode == 'ucb':
+                params[action][1] += 1
                 if args.alpha == 0:
-                    params[0][action] = params[0][action] + (reward - params[0][action])/params[1][action]
+                    params[action][0] += (reward - params[action][0])/params[action][1]
                 else:
-                    params[0][action] = params[0][action] + args.alpha * (reward - params[0][action])
+                    params[action][0] += args.alpha * (reward - params[action][0])
+            elif args.mode == 'gradient':
+                params += args.alpha * reward * (p - d)
 
         rewards[episode] /= steps
 
-
-    # TODO: For every episode, compute its average reward (a single number),
-    # obtaining `args.episodes` values. Then return the final score as
-    # mean and standard deviation of these `args.episodes` values.
-
     return np.mean(rewards), np.std(rewards)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     mean, std = main(parser.parse_args())
     # Print the mean and std for ReCodEx to validate
-    print("{:.2f} {:.2f}".format(mean, std))
+    print('{:.2f} {:.2f}'.format(mean, std))
