23b89db90fcc3de5126a0f9371449d40b074b01d
Jakub Mifek <jakubmifek@gmail.com>
2019-11-24 10:34:13 +0100

    q_tiles init

--- labs/05/q_learning_tiles.py
+++ labs/05/q_learning_tiles.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
-
+from functools import partial
 import mountain_car_evaluator
+
+def get_estimate(state, W):
+    return np.mean([W[i] for i in state], axis=0)
 
 if __name__ == "__main__":
     # Fix random seed
@@ -10,15 +13,15 @@ if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
+    parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
     parser.add_argument("--render_each", default=None, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=None, type=float, help="Learning rate.")
-    parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=None, type=float, help="Exploration factor.")
-    parser.add_argument("--epsilon_final", default=None, type=float, help="Final exploration factor.")
-    parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-    parser.add_argument("--tiles", default=8, type=int, help="Number of tiles.")
+    parser.add_argument("--alpha", default=0.2, type=float, help="Learning rate.")
+    parser.add_argument("--alpha_final", default=0.001, type=float, help="Final learning rate.")
+    parser.add_argument("--epsilon", default=0.5, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon_final", default=0.0001, type=float, help="Final exploration factor.")
+    parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
+    parser.add_argument("--tiles", default=2, type=int, help="Number of tiles.")
     args = parser.parse_args()
 
     # Create the environment
@@ -26,28 +29,43 @@ if __name__ == "__main__":
 
     # Implement Q-learning RL algorithm, using linear approximation.
     W = np.zeros([env.weights, env.actions])
+    print(env.weights) # -- 631 (tiles = 8) ; 307 (tiles = 4)
+    # print(env.states)  -- 631
+    # print(env.actions) -- 3
     epsilon = args.epsilon
     alpha = args.alpha / args.tiles
+    M = 0
 
     evaluating = False
     while not evaluating:
         # Perform a training episode
         state, done = env.reset(evaluating), False
+        
         while not done:
             if args.render_each and env.episode and env.episode % args.render_each == 0:
                 env.render()
 
             # TODO: Choose `action` according to epsilon-greedy strategy
+            estimate = get_estimate(state, W)
+            if np.random.rand() < epsilon:
+                action = np.random.choice(env.actions)
+            else:
+                action = np.argmax(estimate)
 
             next_state, reward, done, _ = env.step(action)
 
             # TODO: Update W values
+            delta = (reward + args.gamma * max(get_estimate(next_state, W)) - estimate[action])
+            for i in state:
+                W[i] += alpha * delta
 
             state = next_state
             if done:
                 break
 
         # TODO: Decide if we want to start evaluating
+        if env.episode >= args.episodes:
+            evaluating = True
 
         if not evaluating:
             if args.epsilon_final:
@@ -60,5 +78,5 @@ if __name__ == "__main__":
         state, done = env.reset(evaluating), False
         while not done:
             # TODO: choose action as a greedy action
-            action = ...
+            action = np.argmax(get_estimate(state, W))
             state, reward, done, _ = env.step(action)
