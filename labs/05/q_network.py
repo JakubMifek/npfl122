61a6cbd4aafabe1e8a005e277e7f2d44e621ceee
Martin Mareš <mmrmartin@gmail.com>
2019-12-02 17:41:30 +0100

    Deep Q-learning: ReCodex version
    Gains reward 500 after around 350 episodes

--- labs/05/q_network.py
+++ labs/05/q_network.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
+#Team members:
+#f9afcdf4-21f5-11e8-9de3-00505601122b
+#90257956-3ea2-11e9-b0fd-00505601122b
+#13926bf3-c4b8-11e8-a4be-00505601122b
+#####################################
+#(Martin Mares)
+#(Jakub Mifek)
+#(Jan Pacovsky)
+
 import collections
+import random
+import sys
 
 import numpy as np
 import tensorflow as tf
@@ -8,34 +19,52 @@ import cart_pole_evaluator
 
 class Network:
     def __init__(self, env, args):
-        # TODO: Create a suitable network
-
         # Warning: If you plan to use Keras `.train_on_batch` and/or `.predict_on_batch`
         # methods, pass `experimental_run_tf_function=False` to compile. There is
         # a bug in TF 2.0 which causes the `*_on_batch` methods not to use `tf.function`.
 
         # Otherwise, if you are training manually, using `tf.function` is a good idea
         # to get good performance.
+        self.train_model = tf.keras.models.Sequential()
+
+        self.train_model.add(tf.keras.Input(shape=env.state_shape, batch_size=args.batch_size))
+        for _ in range(args.hidden_layers):
+            self.train_model.add(tf.keras.layers.Dense(args.hidden_layer_size, activation=tf.nn.relu))
+        self.train_model.add(tf.keras.layers.Dense(env.actions, activation=tf.nn.relu))
+
+        self.train_model.compile(
+            optimizer=tf.optimizers.Adam(
+                learning_rate=args.learning_rate,
+                ),
+            loss=tf.losses.MeanSquaredError(),
+            experimental_run_tf_function=False
+        )
+
+        self.prediction_model = tf.keras.models.clone_model(self.train_model)
 
     # Define a training method. Generally you have two possibilities
     # - pass new q_values of all actions for a given state; all but one are the same as before
     # - pass only one new q_value for a given state, including the index of the action to which
     #   the new q_value belongs
-    def train(self, states, ...):
-        # TODO
-        pass
+    def train(self, states, q_values):
+        self.train_model.train_on_batch(states, q_values)
 
     def predict(self, states):
-        # TODO
-        pass
+        return self.prediction_model.predict(states)
 
+    def update_predictions(self) -> None:
+        """
+        Replace prediction netwok with trained one 
+        """
+        self.prediction_model.set_weights(self.train_model.get_weights())
 
 if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
-    parser.add_argument("--episodes", default=1000, type=int, help="Episodes for epsilon decay.")
+    parser.add_argument("--buffer_size", default=1_000_000, type=int, help="Replay buffer size.")
+    parser.add_argument("--episodes", default=425, type=int, help="Episodes for epsilon decay.")
     parser.add_argument("--epsilon", default=0.3, type=float, help="Exploration factor.")
     parser.add_argument("--epsilon_final", default=0.01, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
@@ -60,37 +89,77 @@ if __name__ == "__main__":
 
     # Replay memory; maxlen parameter can be passed to deque for a size limit,
     # which we however do not need in this simple task.
-    replay_buffer = collections.deque()
+    replay_buffer = collections.deque(maxlen=args.buffer_size)
     Transition = collections.namedtuple("Transition", ["state", "action", "reward", "done", "next_state"])
 
-    evaluating = False
     epsilon = args.epsilon
-    while training:
+    step = 0
+    training = 0
+    rewards_buffer = collections.deque(maxlen=20)
+
+    while (np.mean(rewards_buffer) if len(rewards_buffer) > 0 else 0) < 450:
+        episode_reward = 0
         # Perform episode
         state, done = env.reset(), False
         while not done:
+            step += 1
+
             if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                 env.render()
 
-            # TODO: compute action using epsilon-greedy policy. You can compute
+            # Compute action using epsilon-greedy policy. You can compute
             # the q_values of a given state using
-            #   q_values = network.predict(np.array([state], np.float32))[0]
+            q_values = network.predict(np.array([state], np.float32))[0]
+
+            if random.random() < epsilon:
+                action = np.random.choice(range(env.actions))
+            else:
+                action = np.argmax(q_values)
 
             next_state, reward, done, _ = env.step(action)
+            episode_reward += reward
 
             # Append state, action, reward, done and next_state to replay_buffer
             replay_buffer.append(Transition(state, action, reward, done, next_state))
 
-            # TODO: If the replay_buffer is large enough, preform a training batch
+            # If the replay_buffer is large enough, preform a training batch
             # of `args.batch_size` uniformly randomly chosen transitions.
-            #
-            # After you choose `states` and suitable targets, you can train the network as
-            #   network.train(states, ...)
+            if(step > args.batch_size):
+                training += 1
+
+                # Sample buffer
+                mini_replay = [replay_buffer[idx] for idx in np.random.randint(len(replay_buffer), size=args.batch_size)] 
+
+                # After you choose `states` and suitable targets, you can train the network as
+                #   network.train(states, ...)
+                states = np.array([trasition.state for trasition in mini_replay])
+                actions = np.array([trasition.action for trasition in mini_replay])
+                rewards = np.array([trasition.reward for trasition in mini_replay])
+                dones = np.array([trasition.done for trasition in mini_replay])
+                next_states = np.array([trasition.next_state for trasition in mini_replay])
+
+                q_values = network.predict(states)
+                next_q_values = network.predict(next_states)
+
+                for i in range(args.batch_size):
+                    if dones[i]:
+                        q_values[i,actions[i]] = rewards[i]
+                    else:
+                        q_values[i,actions[i]] = rewards[i] + args.gamma * np.max(next_q_values[i])
+
+                network.train(states, q_values)
+
+                if(training % 10 == 0):
+                    network.update_predictions()
+                    # print(f"Prediction network updated (training={training}, epsilon={epsilon}), buffer={len(replay_buffer)}")
 
             state = next_state
 
         if args.epsilon_final:
             epsilon = np.exp(np.interp(env.episode + 1, [0, args.episodes], [np.log(args.epsilon), np.log(args.epsilon_final)]))
+
+        rewards_buffer.append(episode_reward)
+        # print(f"mean: {np.mean(rewards_buffer) }, reward: {episode_reward}")
 
     # Final evaluation
     while True:
