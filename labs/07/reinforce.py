06f6cd89ce9f07114464297b3019a3c624d3daa8
Jakub Mifek <jakubmifek@gmail.com>
2019-12-07 16:50:21 +0100

    reinforce

--- labs/07/reinforce.py
+++ labs/07/reinforce.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+#Team members:
+#f9afcdf4-21f5-11e8-9de3-00505601122b
+#90257956-3ea2-11e9-b0fd-00505601122b
+#13926bf3-c4b8-11e8-a4be-00505601122b
+#####################################
+#(Martin Mares)
+#(Jakub Mifek)
+#(Jan Pacovsky)
+
 import numpy as np
 import tensorflow as tf
-
+from functools import reduce
+import time
 import cart_pole_evaluator
+
+T = time.time()
 
 class Network:
     def __init__(self, env, args):
-        # TODO: Create a suitable network, using Adam optimizer with given
+        # Create a suitable network, using Adam optimizer with given
         # `args.learning_rate`. The network should predict distribution over
         # possible actions.
 
@@ -16,33 +28,53 @@ class Network:
 
         # Otherwise, if you are training manually, using `tf.function` is a good idea
         # to get good performance.
-        raise NotImplementedError()
+        self.model = tf.keras.models.Sequential()
+        self.model.add(tf.keras.layers.Input(shape=env.state_shape))
+        for _ in range(args.hidden_layers):
+            self.model.add(tf.keras.layers.Dense(args.hidden_layer_size, activation='relu'))
+            # self.model.add(tf.keras.layers.Dropout(0.1))
+        self.model.add(tf.keras.layers.Dense(env.actions, activation='softmax'))
+        
+        self.loss = tf.keras.losses.SparseCategoricalCrossentropy()
 
-    def train(self, states, actions, returns):
-        states, actions, returns = np.array(states, np.float32), np.array(actions, np.int32), np.array(returns, np.float32)
+        self.model.compile(
+            optimizer=tf.optimizers.Adam(learning_rate=args.learning_rate),
+            loss=self.loss,
+            experimental_run_tf_function=False
+        )
 
-        # TODO: Train the model using the states, actions and observed returns.
+        print(self.model.summary())
+
+    def train(self, states, actions, returns, env):        
+        actions = np.array(list(reduce(lambda a, b: np.concatenate((a,b)), actions)))
+        returns = np.array(list(reduce(lambda a, b: np.concatenate((a,b)), returns)))
+        states = np.array(list(reduce(lambda a, b: np.concatenate((a,b)), states)))
+
+        # Train the model using the states, actions and observed returns.
         # Use `returns` as weights in the sparse crossentropy loss.
-        raise NotImplementedError()
+        self.model.train_on_batch(states, actions, sample_weight=returns)
 
     def predict(self, states):
         states = np.array(states, np.float32)
-        # TODO: Predict distribution over actions for the given input states.
-        raise NotImplementedError()
+        return self.model.predict(states)
+
+    def save(self, value):
+        self.model.save('networks/reinforce-{}-{}.model'.format(int(time.time()), value))
 
 
 if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Number of episodes to train on.")
-    parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
-    parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-    parser.add_argument("--hidden_layers", default=None, type=int, help="Number of hidden layers.")
-    parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-    parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
-    parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--batch_size", default=15, type=int, help="Number of episodes to train on.")
+    parser.add_argument("--episodes", default=1000*2, type=int, help="Training episodes.")
+    parser.add_argument("--gamma", default=0.98, type=float, help="Discounting factor.")
+    parser.add_argument("--hidden_layers", default=3, type=int, help="Number of hidden layers.")
+    parser.add_argument("--hidden_layer_size", default=32, type=int, help="Size of hidden layer.")
+    parser.add_argument("--learning_rate", default=0.05, type=float, help="Learning rate.")
+    parser.add_argument("--render_each", default=None, type=int, help="Render some episodes.")
+    parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
+    parser.add_argument('--test', default=30, type=int, help="How often to test the network.")
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
@@ -53,25 +85,36 @@ if __name__ == "__main__":
 
     # Create the environment
     env = cart_pole_evaluator.environment(discrete=False)
+    env2 = cart_pole_evaluator.environment(discrete=False)
+    print(env.actions)
+    print(env.state_shape)
+    print(env.action_shape)
 
     # Construct the network
     network = Network(env, args)
+    A = np.array(range(env.actions))
+    N = args.episodes // args.batch_size
+
+    training = True
 
     # Training
-    for _ in range(args.episodes // args.batch_size):
+    for n in range(N):
         batch_states, batch_actions, batch_returns = [], [], []
         for _ in range(args.batch_size):
             # Perform episode
             states, actions, rewards = [], [], []
             state, done = env.reset(), False
+
             while not done:
                 if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                     env.render()
 
-                # TODO: Compute action probabilities using `network.predict` and current `state`
+                # Compute action probabilities using `network.predict` and current `state`
+                probabilities = network.predict([state])[0]
+                S = sum(probabilities)
 
-                # TODO: Choose `action` according to `probabilities` distribution (np.random.choice can be used)
-
+                # Choose `action` according to `probabilities` distribution (np.random.choice can be used)
+                action = np.random.choice(A, p=probabilities/S)
                 next_state, reward, done, _ = env.step(action)
 
                 states.append(state)
@@ -80,19 +123,59 @@ if __name__ == "__main__":
 
                 state = next_state
 
-            # TODO: Compute returns by summing rewards (with discounting)
+            # Compute returns by summing rewards (with discounting)
 
-            # TODO: Add states, actions and returns to the training batch
+            G = 0
+            Gs = []
+            rewards.reverse()
+            for r in rewards:
+                G = r + args.gamma * G
+                Gs.append(G)
+            Gs.reverse()
+
+            # Add states, actions and returns to the training batch
+
+            batch_states.append(states)
+            batch_actions.append(actions)
+            batch_returns.append(Gs)
+
+            if args.test and env.episode > 0 and np.mean(env._episode_returns[-50:]) > 470 and env.episode % args.test == 0:
+                for _ in range(30):
+                    state, done = env2.reset(), False
+                    while not done:
+                        probabilities = network.predict([state])[0]
+                        action = np.argmax(probabilities)
+                        state, reward, done, _ = env2.step(action)
+                
+                print('Test result: {}'.format(np.mean(env2._episode_returns[-60:])))
+
+                if np.mean(env2._episode_returns[-30:]) > 495:
+                    #network.save(np.mean(env2._episode_returns[-100:]))
+                    print('Training finished')
+                    training = False
+                    break
+
+
+        if not training:
+            break
 
         # Train using the generated batch
-        network.train(batch_states, batch_actions, batch_returns)
+        network.train(
+            batch_states,
+            batch_actions,
+            batch_returns,
+            env
+        )
+        print('Training {}/{} done in {}s'.format(n+1, N, round(time.time() - T, 2)))
 
     # Final evaluation
     while True:
         state, done = env.reset(True), False
         while not done:
-            # TODO: Compute action `probabilities` using `network.predict` and current `state`
+            # Compute action `probabilities` using `network.predict` and current `state`
 
             # Choose greedy action this time
+            probabilities = network.predict([state])[0]
             action = np.argmax(probabilities)
             state, reward, done, _ = env.step(action)
+        # print('Time: {}s'.format(round(time.time() - T, 2)))
