6876a12a83c03ceabaccd32a66237c49b09c2ff8
Jakub Mifek <jakubmifek@gmail.com>
2019-12-17 13:36:35 +0100

    ddpg

--- labs/08/ddpg.py
+++ labs/08/ddpg.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+#Team members:
+#f9afcdf4-21f5-11e8-9de3-00505601122b
+#90257956-3ea2-11e9-b0fd-00505601122b
+#13926bf3-c4b8-11e8-a4be-00505601122b
+#####################################
+#(Martin Mares)
+#(Jakub Mifek)
+#(Jan Pacovsky)
+
 import collections
 
 import numpy as np
@@ -11,14 +20,14 @@ class Network:
         assert len(env.action_shape) == 1
         self.tau = args.target_tau
         action_components = env.action_shape[0]
-        # action_lows, action_highs = map(np.array, env.action_ranges)
-        # action_diff = np.array(list(map(lambda i: action_highs[i] - action_lows[i], range(len(action_lows)))))
+        action_lows, action_highs = map(np.array, env.action_ranges)
+        action_diff = np.array(list(map(lambda i: action_highs[i] - action_lows[i], range(len(action_lows)))))
 
-        # mul_values = tf.multiply(tf.ones((action_components,)), tf.convert_to_tensor(action_diff))
-        # add_values = tf.multiply(tf.ones((action_components,)), tf.convert_to_tensor(action_lows))
+        mul_values = tf.multiply(tf.ones((action_components,)), tf.convert_to_tensor(action_diff))
+        add_values = tf.multiply(tf.ones((action_components,)), tf.convert_to_tensor(action_lows))
 
-        # mul_var = tf.Variable(initial_value=mul_values, trainable=False)
-        # add_var = tf.Variable(initial_value=add_values, trainable=False)
+        mul_var = tf.Variable(initial_value=mul_values, trainable=False)
+        add_var = tf.Variable(initial_value=add_values, trainable=False)
 
         # Create `actor` network, starting with `inputs` and returning
         # `action_components` values for each batch example. Usually, one
@@ -27,12 +36,15 @@ class Network:
         # using `tf.nn.sigmoid` and suitable rescaling.
         input_layer = tf.keras.layers.Input(shape=env.state_shape)
         hidden = tf.keras.layers.Dense(args.hidden_layer, activation='relu')(input_layer)
-        output_layer = tf.keras.layers.Dense(action_components, activation='sigmoid')(hidden)
+        hidden = tf.keras.layers.Dense(action_components, activation='sigmoid')(hidden)
+        output_layer = tf.keras.layers.Multiply()([hidden, mul_var])
+        output_layer = tf.keras.layers.Add()([output_layer, add_var])
 
         self.actor = tf.keras.models.Model(inputs=[input_layer], outputs=[output_layer])
         self.actor.compile(
-            optimizer='adam',
-            loss='mse'
+            optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+            loss='mse',
+            experimental_run_tf_function=False
         )
 
         print(self.actor.summary())
@@ -66,52 +78,28 @@ class Network:
 
     @tf.function
     def _train(self, states, actions, returns):
-        loss = self.critic.train_on_batch((states, actions), returns)
-        
-        actions = self.actor(states, training=True)
-        
-        
-        with tf.GradientTape(watch_accessed_variables=False) as tape:
-            tape.watch(actions)
-            q_values = self.critic((states, actions), training=False)
-            actions = tf.nest.flatten(actions)
+        with tf.GradientTape() as tape:
+            actions = self.actor(states, training=True)
+            values = self.critic((states, actions), training=False)[:, 0]
+            loss = -tf.math.reduce_mean(values)
 
-        dqdas = tape.gradient([q_values], actions)
-
-        with tf.GradientTape(watch_accessed_variables=False) as main_tape:
-            main_tape.watch(actions)
-            actor_losses = []
-            for dqda, action in zip(dqdas, actions):
-                loss = tf.keras.losses.MSE(tf.stop_gradient(dqda + action), action)
-                loss = tf.reduce_mean(loss)
-                actor_losses.append(loss)
-
-            actor_loss = tf.add_n(actor_losses)
-
-        actor_grads = main_tape.gradient(actor_loss, self.actor.variables)
-        self._actor_optimizer.apply_gradients(actor_grads, self.actor.variables)
+        actor_grads = tape.gradient(loss, self.actor.variables)
+        self._actor_optimizer.apply_gradients(zip(actor_grads, self.actor.variables))
 
 
 
 
-        # # TODO: Train separately the actor and critic.
-        # self.critic.train_on_batch((states, actions), returns)
-        # actor_preds = self.actor(states, training=True)
-        # grads = self.critic.gradients((states, actor_preds))
-        # # weights = tf.reshape(self.critic((states, actions),training = False), [-1])
-        # #self.actor.train_on_batch(states, actions) # , sample_weight=weights)
-        # self.actor
+        # TODO: Train separately the actor and critic.
 
-        # # Furthermore, update the weights of the target actor and critic networks
-        # # by using args.target_tau option.
-        # # self.target_actor.set_weights(self.actor.get_weights())
-        # # print('target actor trained')
-        # # self.target_critic.set_weights(self.critic.get_weights())
-        # # print('target critic trained')
+        # Furthermore, update the weights of the target actor and critic networks
+        # by using args.target_tau option.
 
     def train(self, states, actions, returns):
         states, actions, returns = np.array(states, np.float32), np.array(actions, np.float32), np.array(returns, np.float32)
+        self.critic.train_on_batch((states, actions), returns)
+        
         self._train(states, actions, returns)
+
         self.target_actor.set_weights((1-self.tau) * np.array(self.target_actor.get_weights()) + self.tau * np.array(self.actor.get_weights()))
         self.target_critic.set_weights((1-self.tau) * np.array(self.target_critic.get_weights()) + self.tau * np.array(self.critic.get_weights()))
 
@@ -158,31 +146,30 @@ if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=256, type=int, help="Batch size.")
+    parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
     parser.add_argument("--env", default="Pendulum-v0", type=str, help="Environment.")
     parser.add_argument("--evaluate_each", default=10, type=int, help="Evaluate each number of episodes.")
     parser.add_argument("--evaluate_for", default=10, type=int, help="Evaluate for number of batches.")
     parser.add_argument("--noise_sigma", default=0.2, type=float, help="UB noise sigma.")
     parser.add_argument("--noise_theta", default=0.15, type=float, help="UB noise theta.")
-    parser.add_argument("--gamma", default=0.9, type=float, help="Discounting factor.")
-    parser.add_argument("--hidden_layer", default=128, type=int, help="Size of hidden layer.")
-    parser.add_argument("--learning_rate", default=1e-4, type=float, help="Learning rate.")
+    parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
+    parser.add_argument("--hidden_layer", default=50, type=int, help="Size of hidden layer.")
+    parser.add_argument("--learning_rate", default=1e-3, type=float, help="Learning rate.")
     parser.add_argument("--render_each", default=10, type=int, help="Render some episodes.")
-    parser.add_argument("--target_tau", default=1e-3, type=float, help="Target network update weight.")
-    parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--target_tau", default=1e-2, type=float, help="Target network update weight.")
+    parser.add_argument("--threads", default=6, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
-    np.random.seed(42)
-    tf.random.set_seed(42)
+    np.random.seed(128)
+    tf.random.set_seed(128)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create the environment
     env = gym_evaluator.GymEnvironment(args.env)
     action_lows, action_highs = map(np.array, env.action_ranges)
-    print(action_lows, action_highs)
-
+    
     # Construct the network
     network = Network(env, args)
 
@@ -223,7 +210,7 @@ if __name__ == "__main__":
             returns.append(0)
             state, done = env.reset(), False
             while not done:
-                if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
+                if args.render_each and env.episode > 0 and (env.episode+1) % args.render_each == 0:
                     env.render()
 
                 action = network.predict_actions([state])[0]
@@ -234,3 +221,8 @@ if __name__ == "__main__":
             break
 
     # On the end perform final evaluations with `env.reset(True)`
+    for _ in range(100):
+        state, done = env.reset(True), False
+        while not done:
+            action = network.predict_actions([state])[0]
+            state, reward, done, _ = env.step(action)
